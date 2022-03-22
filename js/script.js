d180fbd12dd64ea693034f35bef5e36ebae7a3c8
nwnordahl <nicolaiwnordahl@gmail.com>
2022-03-22 13:21:28 +0100

    Change names and add comments

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
-const footerText = document.querySelector("#footer-text");
+// Select footer paragraph element
+const footerParagraph = document.querySelector("#footer-paragraph");
 
+// Find current year
 const currentDate = new Date();
 const currentYear = currentDate.getFullYear();
 
-footerText.textContent = `Copyright \u00A9 Aktiv Læring ${currentYear}`;
+// Insert text in footer paragraph element
+// '\u00A9' is the unicode for the copyright symbol
+footerParagraph.textContent = `Copyright \u00A9 Aktiv Læring ${currentYear}`;
